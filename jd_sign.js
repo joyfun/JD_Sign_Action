d5959737ea71774da0e86bba164429066db60b0d
joyfun <joyfun@joyfun.net>
2021-01-20 15:26:36 +0800

    add bouns.js

--- jd_sign.js
+++ jd_sign.js
@@ -16,6 +16,7 @@ const push_key = process.env.PUSH_KEY
 
 // 京东脚本文件
 const js_url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
+const js_zns= 'https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_nian.js'
 // 下载脚本路劲
 const js_path = './JD_DailyBonus.js'
 // 脚本执行输出路劲
@@ -108,6 +109,17 @@ function main() {
 
   // 1、下载脚本
   download(js_url, './').then(res=>{
+    // 2、替换cookie
+    setupCookie()
+    // 3、执行脚本
+    exec(`node '${js_path}' >> '${result_path}'`);
+    // 4、发送推送
+    sendNotificationIfNeed() 
+  }).catch((err)=>{
+    console.log('脚本文件下载失败，任务中断！');
+    fs.writeFileSync(error_path, err, 'utf8')
+  })
+  download(js_zns, './').then(res=>{
     // 2、替换cookie
     setupCookie()
     // 3、执行脚本
